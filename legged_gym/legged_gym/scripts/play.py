9b18cf4699bce53954d24c254e4ba320f2ba7c57
Jerry Xu <xhsyjerry@hotmail.com>
2024-05-24 12:12:19 -0400

    remove curriculum in play

--- legged_gym/legged_gym/scripts/play.py
+++ legged_gym/legged_gym/scripts/play.py
@@ -106,16 +106,17 @@ def play(args):
         # "tilt",
     ]
     env_cfg.terrain.BarrierTrack_kwargs["leap"] = dict(
-            length= (1.3, 1.3),
+            length= (1.7, 1.7),
             depth= (0.4, 0.8),
             height= 0.2,
     )
     
     if "one_obstacle_per_track" in env_cfg.terrain.BarrierTrack_kwargs.keys():
         env_cfg.terrain.BarrierTrack_kwargs.pop("one_obstacle_per_track")
-    env_cfg.terrain.BarrierTrack_kwargs["n_obstacles_per_track"] = 2# 2
-    env_cfg.commands.ranges.lin_vel_x = [2.0, 2.0] # [1.2, 1.2]
-    env_cfg.terrain.BarrierTrack_kwargs['track_block_length']= 3.
+    env_cfg.terrain.BarrierTrack_kwargs["n_obstacles_per_track"] = 1# 2
+    env_cfg.commands.curriculum = False
+    env_cfg.commands.ranges.lin_vel_x = [3.0, 3.0] # [1.2, 1.2]
+    env_cfg.terrain.BarrierTrack_kwargs['track_block_length']= 4.
     if "distill" in args.task:
         env_cfg.commands.ranges.lin_vel_x = [0.0, 0.0]
         env_cfg.commands.ranges.lin_vel_y = [-0., 0.]
